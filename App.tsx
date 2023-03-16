8c5bb0483ed84532d38e9d0c6659c03679b68352
Paulo Dias <paulosvdd05@gmail.com>
2023-03-16 10:33:01 -0300

    Fim Calc-ReactNative

--- App.tsx
+++ App.tsx
@@ -20,12 +20,15 @@ export default class App extends Component {
   }
 
   addDigit = n => {
-    if (n === '.' && this.state.displayValue.includes('.')) {
-      return
-    }
+
 
     const clearDisplay = this.state.displayValue === '0'
       || this.state.clearDisplay
+
+    if (n === '.' && !clearDisplay && this.state.displayValue.includes('.')) {
+      return
+    }
+
     const currentValue = clearDisplay ? '' : this.state.displayValue
     const displayValue = currentValue + n
     this.setState({ displayValue, clearDisplay: false })
@@ -38,10 +41,29 @@ export default class App extends Component {
     }
   }
   clearMemory = () => {
-    this.setState({...initialState})
+    this.setState({ ...initialState })
   }
   setOperation = operation => {
+    if (this.state.current === 0) {
+      this.setState({ operation, current: 1, clearDisplay: true })
+    } else {
+      const equals = operation === '='
+      const values = [...this.state.values]
+      try {
+        values[0] = eval(`${values[0]} ${this.state.operation} ${values[1]}`)
+      } catch (e) {
+        values[0] = this.state.values[0]
+      }
 
+      values[1] = 0
+      this.setState({
+        displayValue: `${values[0]}`,
+        operation: equals ? null : operation,
+        current: equals ? 0 : 1,
+        clearDisplay: !equals,
+        values,
+      })
+    }
   }
 
   render() {
