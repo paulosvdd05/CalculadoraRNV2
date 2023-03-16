e62d4defb64a694a35d9b761b9d168ed7207c72b
Paulo Dias <paulosvdd05@gmail.com>
2023-03-16 09:58:19 -0300

    ClearMemory function

--- App.tsx
+++ App.tsx
@@ -8,7 +8,7 @@ const initialState = {
   displayValue: '0',
   clearDisplay: false,
   operation: null,
-  values: [0,0],
+  values: [0, 0],
   current: 0
 }
 
@@ -19,26 +19,26 @@ export default class App extends Component {
     ...initialState
   }
 
-  addDigit = n =>{
-    if(n === '.' && this.state.displayValue.includes('.')){
+  addDigit = n => {
+    if (n === '.' && this.state.displayValue.includes('.')) {
       return
     }
 
     const clearDisplay = this.state.displayValue === '0'
       || this.state.clearDisplay
-      const currentValue = clearDisplay ? '' : this.state.displayValue
-      const displayValue = currentValue + n
-      this.setState({displayValue, clearDisplay:false})
+    const currentValue = clearDisplay ? '' : this.state.displayValue
+    const displayValue = currentValue + n
+    this.setState({ displayValue, clearDisplay: false })
 
-      if (n !== '.'){
-        const newValue = parseFloat(displayValue)
-        const values = [...this.state.values]
-        values[this.state.current] = newValue
-        this.setState({values})
-      }
+    if (n !== '.') {
+      const newValue = parseFloat(displayValue)
+      const values = [...this.state.values]
+      values[this.state.current] = newValue
+      this.setState({ values })
+    }
   }
-  clearMemory = () =>{
-    this.setState({displayValue: '0'})
+  clearMemory = () => {
+    this.setState({...initialState})
   }
   setOperation = operation => {
 
@@ -49,23 +49,23 @@ export default class App extends Component {
       <View style={styles.container}>
         <Display value={this.state.displayValue} />
         <View style={styles.buttons}>
-          <Button label='AC' triple onClick={this.clearMemory}/>
-          <Button label='/' operation onClick={this.setOperation}/>
-          <Button label='7' onClick={this.addDigit}/>
-          <Button label='8' onClick ={this.addDigit}/>
-          <Button label='9' onClick ={this.addDigit}/>
-          <Button label='*' operation onClick={this.setOperation}/>
-          <Button label='4' onClick ={this.addDigit}/>
-          <Button label='5' onClick ={this.addDigit}/>
-          <Button label='6' onClick ={this.addDigit}/>
-          <Button label='-' operation onClick={this.setOperation}/>
-          <Button label='1' onClick ={this.addDigit}/>
-          <Button label='2' onClick ={this.addDigit}/>
-          <Button label='3' onClick ={this.addDigit}/>
-          <Button label='+' operation onClick={this.setOperation}/>
-          <Button label='0' double onClick ={this.addDigit}/>
-          <Button label='.' onClick ={this.addDigit}/>
-          <Button label='=' operation onClick={this.setOperation}/>
+          <Button label='AC' triple onClick={this.clearMemory} />
+          <Button label='/' operation onClick={this.setOperation} />
+          <Button label='7' onClick={this.addDigit} />
+          <Button label='8' onClick={this.addDigit} />
+          <Button label='9' onClick={this.addDigit} />
+          <Button label='*' operation onClick={this.setOperation} />
+          <Button label='4' onClick={this.addDigit} />
+          <Button label='5' onClick={this.addDigit} />
+          <Button label='6' onClick={this.addDigit} />
+          <Button label='-' operation onClick={this.setOperation} />
+          <Button label='1' onClick={this.addDigit} />
+          <Button label='2' onClick={this.addDigit} />
+          <Button label='3' onClick={this.addDigit} />
+          <Button label='+' operation onClick={this.setOperation} />
+          <Button label='0' double onClick={this.addDigit} />
+          <Button label='.' onClick={this.addDigit} />
+          <Button label='=' operation onClick={this.setOperation} />
         </View>
       </View>
     )
@@ -73,11 +73,11 @@ export default class App extends Component {
 }
 
 const styles = StyleSheet.create({
-  container:{
-    flex:1
+  container: {
+    flex: 1
   },
-  buttons:{
-    flexDirection:'row',
-    flexWrap:'wrap'
+  buttons: {
+    flexDirection: 'row',
+    flexWrap: 'wrap'
   }
 })
